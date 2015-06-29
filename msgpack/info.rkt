e0333d5508caaf4d28f5d37069859cbcd0bf5d75
Erik Nyhus <eriknyh@stud.ntnu.no>
2015-06-29 17:51:26 +0200

    Add msgpack meta package

---
+++ msgpack/info.rkt
@@ -0,0 +1,6 @@
+#lang info
+
+(define collection 'multi)
+(define deps '("base"
+               "git://github.com/eriknyh/msgpack-racket?path=msgpack-lib"))
+(define implies '("git://github.com/eriknyh/msgpack-racket?path=msgpack-lib"))
