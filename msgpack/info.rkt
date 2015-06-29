d47eb19340d0f93b3da17ecec7b6b68ec68cbcfb
Erik Nyhus <eriknyh@stud.ntnu.no>
2015-06-29 17:53:25 +0200

    Change implies statement in msgpack/info.rkt

--- msgpack/info.rkt
+++ msgpack/info.rkt
@@ -3,4 +3,4 @@
 (define collection 'multi)
 (define deps '("base"
                "git://github.com/eriknyh/msgpack-racket?path=msgpack-lib"))
-(define implies '("git://github.com/eriknyh/msgpack-racket?path=msgpack-lib"))
+(define implies '("msgpack-lib"))
